1d8052d045d131955f067596b940f7f2510dc940
Jay <jai.desale20052005@gmail.com>
2022-05-11 12:42:57 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,8 +1,8 @@
 import socket
 import json
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('localhost', 50000))
+
+
 Current_BTC_Rate = "100" #Hypothetical 1BTC = $100
 
 def LP_crypto_to_fiat(input_btc):
@@ -33,9 +33,7 @@ def LP_crypto_to_fiat(input_btc):
     print("Updated")
     
 
-
-
-def crypto_to_fiat():
+def crypto_to_fiat_initial():
     print("Welcome to Crypto=>Fiat")
     inpp_sell = input("Please input how much BTC you want to sell?: ")
     continue_yes_or_no = input("1BTC = $100, Would you like to continue?[Y/N]: ")
@@ -55,30 +53,43 @@ def crypto_to_fiat():
 
     LP_crypto_to_fiat(inpp_sell)
 
-def fiat_to_crypto():
+def fiat_to_crypto(): #To be written
     print("Fiat=>Crypto")
-    inpp_sell = input("How much BTC would you like to buy?: ")
-    #processing currency
-    fi = open('Wallet.json')
-    Cur_data = json.load(fi)
-    old_amt = Cur_data["USD"]
-    Coinfees = int(inpp_sell) * int(Current_BTC_Rate) 
-    new_amt = int(old_amt) - int(Coinfees)
-    updatedcurrencyobj = Cur_data
-    updatedcurrencyobj["USD"] = new_amt
-    with open("Wallet.json", "w") as outfile:
-        json.dump(updatedcurrencyobj, outfile)
+    # inpp_sell = input("How much BTC would you like to buy?: ")
+    # #processing currency
+    # fi = open('Wallet.json')
+    # Cur_data = json.load(fi)
+    # old_amt = Cur_data["USD"]
+    # Coinfees = int(inpp_sell) * int(Current_BTC_Rate) 
+    # new_amt = int(old_amt) - int(Coinfees)
+    # updatedcurrencyobj = Cur_data
+    # updatedcurrencyobj["USD"] = new_amt
+    # with open("Wallet.json", "w") as outfile:
+    #     json.dump(updatedcurrencyobj, outfile)
+
+
+if __name__ == "__main__":
+
+    while 1:
+        #Keep trying untill a LP provider is available to connect to
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(('localhost', 50000))
+            inpp = input("[1]Crypto-To-Fiat\n""[2]Fiat-To-Crypto\nEnter: ")
+        
+            if "1" in inpp:
+                crypto_to_fiat_initial()
+
+            elif "2" in inpp:
+                fiat_to_crypto()
+
+            else:
+                print("something went wrong!")
+
+        except:
+             LP_exists = False
+             print(LP_exists)
 
 
 
-
-
-inpp = input("[1]Crypto-To-Fiat\n""[2]Fiat-To-Crypto\nEnter: ")
-if "1" in inpp:
-    crypto_to_fiat()
-
-elif "2" in inpp:
-    fiat_to_crypto()
-
-else:
-    print("something went wrong!")
+    
